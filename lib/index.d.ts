5bae407e346a6120366e7d76a4c586ac9eb8ded0
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-19 22:17:08 +0800

    优化

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,8 +2,5 @@
 import http from "http";
 import https from "https";
 declare type ServerOptions = https.ServerOptions;
-declare function createServer(
-    config: ServerOptions,
-    requestListener: http.RequestListener
-): https.Server;
+declare function createServer(config: ServerOptions, requestListener: http.RequestListener): https.Server;
 export { createServer };
--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import http from "http";
 import https from "https";
 import net from "net";
 import spdy from "spdy";
-// import { Duplex } from "stream";
+const eventproto = events.EventEmitter.prototype;
 type ServerOptions = https.ServerOptions;
 
 function createServer(
@@ -15,12 +15,10 @@ function createServer(
     }
     const serverhttp = http.createServer(config, requestListener);
     const serverspdy = spdy.createServer(config, requestListener);
-
+    eventproto.removeAllListeners.call(serverhttp, "request");
+    eventproto.removeAllListeners.call(serverspdy, "request");
     const onconnection = serverspdy.listeners("connection");
-    events.EventEmitter.prototype.removeAllListeners.call(
-        serverspdy,
-        "connection"
-    );
+    eventproto.removeAllListeners.call(serverspdy, "connection");
     function handletls(socket: net.Socket) {
         onconnection.forEach((listeners) => listeners.call(serverspdy, socket));
     }
@@ -29,80 +27,10 @@ function createServer(
     }
     const connectionListener = function (socket: net.Socket) {
         socket.on("error", function onError() {});
-        // let firsthandle = true;
+
         let ishttp = false;
         let istls = false;
 
-        // const streamhttp = new Duplex({
-        //     write(
-        //         chunk: any,
-        //         encoding: string,
-        //         callback: (error?: Error | null | undefined) => void
-        //     ) {},
-        //     writev(
-        //         chunks: {
-        //             chunk: any;
-        //             encoding: string;
-        //         }[],
-        //         callback: (error?: Error | null | undefined) => void
-        //     ) {},
-
-        //     final(callback: (error?: Error | null | undefined) => void) {},
-        //     read(size: number) {},
-        //     destroy(
-        //         error: Error | null,
-        //         callback: (error: Error | null) => void
-        //     ) {},
-        // });
-
-        // const streamtls = new Duplex({
-        //     write(
-        //         chunk: any,
-        //         encoding: string,
-        //         callback: (error?: Error | null | undefined) => void
-        //     ) {},
-        //     writev(
-        //         chunks: {
-        //             chunk: any;
-        //             encoding: string;
-        //         }[],
-        //         callback: (error?: Error | null | undefined) => void
-        //     ) {},
-
-        //     final(callback: (error?: Error | null | undefined) => void) {},
-        //     read(size: number) {},
-        //     destroy(
-        //         error: Error | null,
-        //         callback: (error: Error | null) => void
-        //     ) {},
-        // });
-
-        // Reflect.set(streamhttp, "write", function (
-        //     chunk: string | Uint8Array,
-        //     encoding: string | undefined,
-        //     callback: ((err?: Error | undefined) => void) | undefined
-        // ) {
-        //     if (ishttp) {
-        //         console.log("http data write", chunk);
-        //         return socket.write(chunk, encoding, callback);
-        //     } else {
-        //         callback?.();
-        //         return true;
-        //     }
-        // });
-        // Reflect.set(streamtls, "write", function (
-        //     chunk: string | Uint8Array,
-        //     encoding: string | undefined,
-        //     callback: ((err?: Error | undefined) => void) | undefined
-        // ) {
-        //     if (istls) {
-        //         console.log("tls data write", chunk);
-        //         return socket.write(chunk, encoding, callback);
-        //     } else {
-        //         callback?.();
-        //         return true;
-        //     }
-        // });
         const data = socket.read(1);
         /* https://github.com/httptoolkit/httpolyglot/blob/master/lib/index.js */
         if (data === null) {
@@ -116,6 +44,8 @@ function createServer(
                 istls = true;
             } else if (32 < firstByte && firstByte < 127) {
                 ishttp = true;
+            } else {
+                socket.destroy();
             }
             if (ishttp) {
                 handlehttp(socket);
@@ -125,40 +55,8 @@ function createServer(
             }
         }
         /* 测试发现不能使用on data事件,会收不到响应 */
-        // socket.once("data", (data) => {
-        //     if (firsthandle) {
-        //         firsthandle = false;
-        //         // const firstByte = data[0];
-        //         // socket.unshift(data);
-        //         // if (firstByte === 22) {
-        //         //     istls = true;
-        //         // } else if (32 < firstByte && firstByte < 127) {
-        //         //     ishttp = true;
-        //         // }
-        //         // if (ishttp) {
-        //         //     serverhttp.emit("connection", socket);
-        //         // }
-        //         // if (istls) {
-        //         //     onconnection.forEach((listeners) =>
-        //         //         listeners.call(serverspdy, socket)
-        //         //     );
-        //         // }
-        //     }
-        //     // if (ishttp) {
-        //     //     streamhttp.emit("data", data);
-        //     //     console.log("http data recv", data);
-        //     // }
-        //     // if (istls) {
-        //     //     streamtls.emit("data", data);
-        //     //     console.log("tls data recv", data);
-        //     // }
-        // });
     };
-    events.EventEmitter.prototype.on.call(
-        serverspdy,
-        "connection",
-        connectionListener
-    );
+    eventproto.addListener.call(serverspdy, "connection", connectionListener);
 
     return serverspdy;
 }
