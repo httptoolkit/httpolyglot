eb50c722863465537fe8e0b4c022d9a3a1ce46be
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-19 17:08:29 +0800

    测试

--- test/one.js
+++ test/one.js
@@ -2,6 +2,11 @@ import { createServer } from "../lib/index.js";
 import fs from "fs";
 const port = 9000;
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 createServer(
     {
         key: fs.readFileSync(path.join(__dirname, "server.key")),
--- test/two.js
+++ test/two.js
@@ -1,6 +1,11 @@
 import { createServer } from "../lib/index.js";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const port = 9001;
 createServer(
     {
