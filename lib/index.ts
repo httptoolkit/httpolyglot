eef3695537f96800d61e9bb6b5a66ed898f77152
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-19 19:53:13 +0800

    优化

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import http from "http";
 import https from "https";
 import net from "net";
 import spdy from "spdy";
-
+import { Duplex } from "stream";
 type ServerOptions = https.ServerOptions;
 
 function createServer(
@@ -26,35 +26,38 @@ function createServer(
         let firsthandle = true;
         let ishttp = false;
         let istls = false;
-        const streamhttp = new net.Socket();
-        const streamtls = new net.Socket();
+        const streamhttp = new Duplex();
+        const streamtls = new Duplex();
+        Reflect.set(streamhttp, "write", function (
+            chunk: string | Uint8Array,
+            encoding: string | undefined,
+            callback: ((err?: Error | undefined) => void) | undefined
+        ) {
+            debugger;
+            if (ishttp) {
+                return socket.write(chunk, encoding, callback);
+            } else {
+                callback?.();
+                return true;
+            }
+        });
+        Reflect.set(streamtls, "write", function (
+            chunk: string | Uint8Array,
+            encoding: string | undefined,
+            callback: ((err?: Error | undefined) => void) | undefined
+        ) {
+            debugger;
+            if (istls) {
+                return socket.write(chunk, encoding, callback);
+            } else {
+                callback?.();
+                return true;
+            }
+        });
 
-        streamhttp.on("data", (data) => {
-            if (ishttp) {
-                socket.write(data);
-            }
-        });
-        streamhttp.on("end", () => {
-            if (ishttp) {
-                socket.end();
-            }
-        });
-        streamtls.on("data", (data) => {
-            if (istls) {
-                socket.write(data);
-            }
-        });
-        streamtls.on("end", () => {
-            if (istls) {
-                socket.end();
-            }
-        });
-        serverhttp.emit("connection", streamhttp);
-        onconnection.forEach((listeners) =>
-            listeners.call(serverspdy, streamtls)
-        );
         socket.on("error", function onError() {});
         socket.on("data", (data) => {
+            debugger;
             if (firsthandle) {
                 firsthandle = false;
                 const firstByte = data[0];
@@ -63,12 +66,20 @@ function createServer(
                 } else if (32 < firstByte && firstByte < 127) {
                     ishttp = true;
                 }
+                if (ishttp) {
+                    serverhttp.emit("connection", streamhttp);
+                }
+                if (istls) {
+                    onconnection.forEach((listeners) =>
+                        listeners.call(serverspdy, streamtls)
+                    );
+                }
             }
             if (ishttp) {
-                streamhttp.write(data);
+                streamhttp.emit("data", data);
             }
             if (istls) {
-                streamtls.write(data);
+                streamtls.emit("data", data);
             }
         });
     };
@@ -81,4 +92,3 @@ function createServer(
     return serverspdy;
 }
 export { createServer };
-
