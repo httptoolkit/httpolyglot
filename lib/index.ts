eafd7f2b90fb4a9c62828ca530ec05ba9abd4363
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-19 16:58:02 +0800

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import net from "net";
 import spdy from "spdy";
 import events from "events";
 import https from "https";
-
+import { inherits } from "util";
 type ServerOptions = http.ServerOptions & https.ServerOptions;
 class Server extends net.Server {
     constructor(config: ServerOptions, requestListener: http.RequestListener) {
@@ -45,7 +45,7 @@ class Server extends net.Server {
         }
     }
 }
-
+inherits(Server, https.Server);
 function createServer(
     config: ServerOptions,
     requestListener: http.RequestListener
