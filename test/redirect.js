39ecb18fc1ce0c73b8d301fa1f0ff6927a218e4f
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-20 13:00:53 +0800

    Update redirect.js

--- test/redirect.js
+++ test/redirect.js
@@ -7,7 +7,7 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const port = 9001;
-createServer(
+const server=createServer(
     {
         key: fs.readFileSync(path.join(__dirname, "server.key")),
         cert: fs.readFileSync(path.join(__dirname, "server.crt")),
@@ -25,6 +25,8 @@ createServer(
             return res.end();
         }
     }
-).listen(port, "localhost", function () {
+)
+
+server.listen(port, "localhost", function () {
     console.log("httpolyglot server listening on port " + port);
 });
