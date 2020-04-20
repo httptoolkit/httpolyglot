415982cfbbefdc37ce613944ac3fab7896f678c0
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-20 21:09:59 +0800

    重构fetch

--- test/fetch1.js
+++ test/fetch1.js
@@ -5,23 +5,33 @@ import { urls } from "./urls.js";
 
 process.on("unhandledRejection", console.error);
 
+const fetch =
+    /**
+     * @param {string} url
+     * @param {Object} opt
+     */
+    function (url, opt) {
+        const agent = new https.Agent({
+            ca: cert,
+        });
+        // @ts-ignore
+        return fetch1.default(url, {
+            agent: url.startsWith("http:") ? undefined : agent,
+            ...opt,
+        });
+    };
 
 // @ts-ignore
 
 ~((fetch) => {
-    const agent = new https.Agent({
-        ca: cert,
-    });
     Promise.all(
         urls
             .map((url) => {
                 return fetch(url, {
                     redirect: "manual",
-                    agent: url.startsWith("http:") ? undefined : agent,
                 }).then((r) => Promise.all([r, r.text()]));
             })
             .flat(1 / 0)
     ).then(console.log);
     // @ts-ignore
-})(fetch1.default);
-
+})(fetch);
