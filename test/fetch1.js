a63ea6e6d76e87a8beeee2f523a6c0d4de9a8a4b
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-20 21:05:52 +0800

    拆分模块

--- test/fetch1.js
+++ test/fetch1.js
@@ -1,11 +1,10 @@
+import https from "https";
+import fetch1 from "node-fetch";
+import { cert } from "./key-cert.js";
 import { urls } from "./urls.js";
 
 process.on("unhandledRejection", console.error);
-import https from "https";
-import fetch1 from "node-fetch";
 
-import fetch2 from "fetch-h2";
-import { cert } from "./key-cert.js";
 
 // @ts-ignore
 
@@ -26,15 +25,3 @@ import { cert } from "./key-cert.js";
     // @ts-ignore
 })(fetch1.default);
 
-~((fetch) => {
-    Promise.all(
-        urls
-            .map((url) => {
-                return fetch(url, {
-                    redirect: "manual",
-                }).then((r) => Promise.all([r, r.text()]));
-            })
-            .flat(1 / 0)
-    ).then(console.log);
-    // @ts-ignore
-})(fetch2.context({ session: { ca: cert } }).fetch);
---
+++ test/fetch2.js
@@ -0,0 +1,21 @@
+import fetch2 from "fetch-h2";
+import { cert } from "./key-cert.js";
+import { urls } from "./urls.js";
+
+process.on("unhandledRejection", console.error);
+
+
+// @ts-ignore
+
+~((fetch) => {
+    Promise.all(
+        urls
+            .map((url) => {
+                return fetch(url, {
+                    redirect: "manual",
+                }).then((r) => Promise.all([r, r.text()]));
+            })
+            .flat(1 / 0)
+    ).then(console.log);
+    // @ts-ignore
+})(fetch2.context({ session: { ca: cert } }).fetch);
