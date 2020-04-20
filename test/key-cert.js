f8014378af0adaf738f156ceab6bc9c841d22124
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-20 20:21:53 +0800

    Update key-cert.js

--- test/key-cert.js
+++ test/key-cert.js
@@ -5,5 +5,5 @@ import { dirname } from "path";
 // @ts-ignore
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-export const key = fs.readFileSync(path.join(__dirname, "server.key.pem"));
-export const cert = fs.readFileSync(path.join(__dirname, "server.crt.pem"));
+export const key = fs.readFileSync(path.join(__dirname, "../","server.key.pem"));
+export const cert = fs.readFileSync(path.join(__dirname, "../","server.crt.pem"));
