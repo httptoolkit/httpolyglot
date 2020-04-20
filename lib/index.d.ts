0746555af3eac0a4ac57d45af43e0c6007fb29c5
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-20 21:00:57 +0800

    美化

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,6 @@
 /// <reference types="node" />
 import net from "net";
-import {
-    RequestListener,
-    ServerOptions,
-    UpgradeListener,
-} from "./declaration.js";
+import { RequestListener, ServerOptions, UpgradeListener } from "./declaration.js";
 export * from "./declaration.js";
 export { createServer };
-declare function createServer(
-    config: ServerOptions,
-    requestListener?: RequestListener,
-    upgradeListener?: UpgradeListener
-): net.Server;
+declare function createServer(config: ServerOptions, requestListener?: RequestListener, upgradeListener?: UpgradeListener): net.Server;
