c26390cc7424ab18180c5e0d13e402bf826ff7c8
masx200 <34191203+masx200@users.noreply.github.com>
2020-04-21 09:57:29 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -6,8 +6,18 @@ if (url.protocol === "https:") {
 }
 
 let socket = new WebSocket(url.href);
-
+/**
+ * @param {string} message
+ */
+function appendtodom(message) {
+    let p = document.createElement("p");
+    p.innerText = message;
+    document.body.appendChild(p);
+}
 socket.addEventListener("close", console.log);
 socket.addEventListener("open", console.log);
 socket.addEventListener("error", console.log);
 socket.addEventListener("message", console.log);
+socket.addEventListener("message", (e) => {
+    appendtodom(e.data);
+});
